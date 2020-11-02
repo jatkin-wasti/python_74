9aed899d668a0aa337a319dad33be4c73f9d23a3
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-02 15:05:29 +0000

    added code and comments into python variables file

--- README.md
+++ README.md
@@ -9,5 +9,21 @@
 - Allows you to scale even the most complex applications with ease 
 - Extensive support libraries 
 ## pycharm setup
+- Can highlight multiple lines in the editor and press cntrl + / to indent all of the lines
 ### python 3.7
 ### Python Variable
+
+- What is a variable
+- Dynamically typed languages
+- Overwriting variables
+- Getting user input from the command line
+
+- Types of variables in python:
+- Int, Float, String, Boolean
+
+## Variable:
+- is a place holder to store data
+
+## Pseudocode:
+- writing what you want the program to do in natural language
+- this pseudocode can be translated into actual code
--- python_variables.py
+++ python_variables.py
@@ -1,1 +1,33 @@
-print("Hello World")
+# How can we create a variable
+# print("Jamie")
+
+# create a variable called name to store users name as a string
+name = "Jamie" # declaring string variable
+# creating a variable called age to store users age as an int
+age = 23 # declaring integer variable
+# creating a variable called hourly_wage to store users hourly wage as a float
+hourly_wage = 10.50 # declaring float variable
+# creating a variable called travel_allowance to store users travel allowance as a float
+travel_allowance = 2.4 # declaring float variable
+# This series of print statements output the values stored in the given variables
+print(name)
+print(hourly_wage)
+print(travel_allowance)
+print(age)
+
+# How can we find out the type of a variable
+# We have the type() method in python that gives us the actual data type stored in a variable
+print(type(age)) # this returns class int because the data stored in the age variable is an int
+
+# How can we take user data
+# We can use a method called input() to get data from the user
+name = input("Please enter your name:  ")
+# Then we are displaying a welcome message using the name that the user input
+print(f"Hi, {name}!")
+
+# Receive data from users
+input()
+# Display user data on command line
+print()
+# Save data in a csv file
+#openwith()
