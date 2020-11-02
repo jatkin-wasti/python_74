27ea13b36bf0ee55b144d2f2125ac9fc3f31cbad
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-02 16:28:56 +0000

    added the completed task given for strings, concatenation, and casting

--- strings_concatenation_&_casting.py
+++ strings_concatenation_&_casting.py
@@ -18,3 +18,16 @@ print(first_name, middle_name, last_name)
 print(first_name + ' ' + middle_name + ' ' + last_name + ' is ' + str(age) + ' years old.')
 num_as_string = "007"  # This string contains a number but we may want to use it as an int
 print(7 + int(num_as_string))  # Therefore we can cast it as an int to use it like one
+
+
+name = input("Please enter your full name:  ")
+dob = input("Please enter your date of birth in DD/MM/YYYY format:  ")
+age = input("Please enter your age:  ")
+post_code = input("Please enter your post code:  ")
+first_line_address = input("Please enter the first line of your address:  ")
+house_number = input("Please enter your house number:  ")
+print("Your name is " + name + ".\n"
+                               " Your age is " + str(age) +
+                               ".\nYour date of birth is " + dob +
+                               ".\nYour address is " + str(house_number) +
+                               " " + first_line_address + ", " + str(post_code))
